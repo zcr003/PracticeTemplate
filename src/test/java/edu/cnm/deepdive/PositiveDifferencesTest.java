a37393a49df1f6873e436d24c7a4d2e60562bf53
zcr003 <zramsey1@cnm.edu>
2021-12-02 17:33:48 -0700

    Attempting parameterized test for Positive Difference

---
+++ src/test/java/edu/cnm/deepdive/PositiveDifferencesTest.java
@@ -0,0 +1,31 @@
+package edu.cnm.deepdive;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+class PositiveDifferencesTest {
+
+  @DisplayName("Testing PositiveDifferences")
+  @ParameterizedTest
+  @CsvFileSource(resources = "data.csv")
+   static int amountOfDifference(int[] input) {
+   int positiveDifference = countPositiveDifferences(input);
+   return positiveDifference;
+  }
+
+
+  public static int countPositiveDifferences(int[] data) {
+    int positiveDifferences = 0;
+
+    for (int i = 0; i < data.length - 1; i++) {
+      if ((data[i + 1] - data[i]) > 0) {
+        positiveDifferences++;
+      }
+    }
+    return positiveDifferences;
+  }
+
+}
