8a738cf625b9e2500f81b8c6db59e891c9337ed3
zcr003 <zramsey1@cnm.edu>
2021-12-02 19:56:46 -0700

    Working on 2nd case.

--- src/test/java/edu/cnm/deepdive/PositiveDifferencesTest.java
+++ src/test/java/edu/cnm/deepdive/PositiveDifferencesTest.java
@@ -7,25 +7,25 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
 class PositiveDifferencesTest {
-
-  @DisplayName("Testing PositiveDifferences")
-  @ParameterizedTest
-  @CsvFileSource(resources = "data.csv")
-   static int amountOfDifference(int[] input) {
-   int positiveDifference = countPositiveDifferences(input);
-   return positiveDifference;
-  }
-
-
-  public static int countPositiveDifferences(int[] data) {
-    int positiveDifferences = 0;
-
-    for (int i = 0; i < data.length - 1; i++) {
-      if ((data[i + 1] - data[i]) > 0) {
-        positiveDifferences++;
-      }
-    }
-    return positiveDifferences;
-  }
+//
+//  @DisplayName("Testing PositiveDifferences")
+//  @ParameterizedTest
+//  @CsvFileSource(resources = "data.csv")
+//   static int amountOfDifference(int[] input) {
+//   int positiveDifference = countPositiveDifferences(input);
+//   return positiveDifference;
+//  }
+//
+//
+//  public static int countPositiveDifferences(int[] data) {
+//    int positiveDifferences = 0;
+//
+//    for (int i = 0; i < data.length - 1; i++) {
+//      if ((data[i + 1] - data[i]) > 0) {
+//        positiveDifferences++;
+//      }
+//    }
+//    return positiveDifferences;
+//  }
 
 }
